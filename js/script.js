17dd39be440e720552d294f5e2e02a1175288b96
mayankhooda <mayankhooda@google.com>
2021-11-13 04:24:35 +0530

    fix petals

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-/**
- * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
- */
 (function ($) {
     "use strict";
       $('.sakura-falling').sakura();
 })(jQuery);
-
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
@@ -22,6 +18,7 @@ $(document).on('click', function(){
     }
     audioElement.play();
     $("div.cover").fadeOut(3000, function() {
+        $("div.cover").remove();
         console.log("ho gya stop");
     });
 });
@@ -39,35 +36,6 @@ $(document).on('visibilitychange', function() {
     }
 })
 
-// Set the date we're counting down to
-var countDownDate = new Date("Dec 12, 2021 20:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // // Output the result in an element with id="demo"
-    // document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
-    }
-}, 1000);
-
-// being a bit cool :p  
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
